35b942fdf9be186808e37669150adea915eb778b
Andrey Sergeyev <savilit@yandex.ru>
2023-09-10 21:41:12 +0300

    fix: Table row now render left and right border of cells by default

--- src/TableRow.tsx
+++ src/TableRow.tsx
@@ -87,8 +87,10 @@ export const TableRow = <T,>(props: Partial<TableRowProps<T>>) => {
               key: columnIndex,
               fontSize: rc.props.fontSize || props.fontSize,
               textAlign: rc.props.textAlign || props.textAlign,
-              includeLeftBorder: (rc.props.includeLeftBorder || true) && columnIndex === 0,
-              includeRightBorder: (rc.props.includeRightBorder || true) && includeRightBorder && columnIndex !== (rowCells.length - 1)
+              includeLeftBorder: columnIndex === 0,
+              includeRightBorder: columnIndex !== (rowCells.length - 1),
+      // includeLeftBorder: (rc.props.includeLeftBorder || true) && columnIndex === 0,
+      //         includeRightBorder: (rc.props.includeRightBorder || true) && includeRightBorder && columnIndex !== (rowCells.length - 1)
             }))
           }
       </View>
